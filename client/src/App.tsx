8817a08b3d6cf1de661ddcdc38ba52e9f2506ce5
Celal Yasin Nari <celalyasinnari@gmail.com>
2023-02-10 15:44:19 +0200

    mapping

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,47 @@
-import React, { Fragment} from 'react';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import Match from "./pages/Match";
+import ToDo from "./pages/ToDo";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Fragment>
-      
+      <BrowserRouter>
+        <div className="flex flex-col h-screen justify-between">
+          <Navbar />
+          <div className="flex flex-col justify-content">
+            <Routes>
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/register"
+                element={<Register />}
+              />
+              <Route
+                path="/main"
+                element={<Main />}
+              />
+              <Route
+                path="/todo"
+                element={<ToDo />}
+              />
+              <Route
+                path="/match"
+                element={<Match />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </Fragment>
   );
 }
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const access_token = localStorage.getItem("user");
+  const token = access_token != null ? JSON.parse(access_token) : "";
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("access_token");
+    navigate("/login");
+  };
+
+  return (
+    <div className="bg-black w-full py-2 px-4 lg:px-8 lg:py-4 flex items-center justify-between text-gray-900">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <Link
+          to={`/main`}
+          className="text-white mr-4 cursor-pointer py-1  font-bold text-lg">
+          <span>Slush Match & To-Do</span>
+        </Link>
+        <ul className="ml-5 mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+          <li className="text-sm text-white p-1 font-normal">
+            <Link
+              to={`/match`}
+              className="flex items-center hover:text-gray-500">
+              Match
+            </Link>
+          </li>
+          <li className="text-sm text-white p-1 font-normal">
+            <Link
+              to={`/todo`}
+              className="flex items-center hover:text-gray-500">
+              To-Do
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="flex items-center md:ml-12">
+        {token.userToken ? (
+          <button
+            onClick={handleLogout}
+            className="ml-8 inline-flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-indigo-800 shadow-sm hover:bg-gray-300">
+            Log out
+          </button>
+        ) : (
+          <>
+            <Link
+              to={"/login"}
+              className="text-base font-medium text-white hover:text-gray-500">
+              Sign in
+            </Link>
+            <Link
+              to={"/register"}
+              className="ml-8 inline-flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-indigo-800 shadow-sm hover:bg-gray-300">
+              Sign up
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
