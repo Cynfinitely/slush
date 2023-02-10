10c37bb43806d0632fecbb09da9d8aa5bdf1435a
Celal Yasin Nari <celalyasinnari@gmail.com>
2023-02-10 16:10:09 +0200

    navbar footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Slush from "../assets/slush.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-black w-full py-2 px-4 lg:px-8 lg:py-4 flex items-center justify-between text-white">
+      <Link
+        to={`https://github.com/Cynfinitely`}
+        className="flex items-center hover:text-gray-500">
+        dev:CYN
+      </Link>
+      <img
+        src={Slush}
+        alt="Logo"
+        className="w-fit h-7"></img>
+      <p>All Rights Reserved &copy;</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
